0bc34374d4a5e13e149e4159ae1b41659b1a7f9c
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2023-10-25 20:16:07 +0600

    1st

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Center(
+        child: CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.blueAccent,
+          backgroundImage: AssetImage('#'),
+          child: Text('Hi',style: TextStyle(color: Colors.white),),
+        ),
+      ),
+    ));
+  }
+}
+
+
